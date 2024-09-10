14c0c22559e5484adfb84eab34d0549f59f71276
Abhishek Kachhawa <www.abhishek0kachhawa@gmail.com>
2024-09-10 20:40:13 +0530

    PPH

---
+++ fresh/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from './components/Footer'
+import Main from './components/Main'
+import AboutSection from './components/AboutSection'
+
+
+const App = () => {
+  return (
+    <div>
+      <Main/>
+      <AboutSection/>
+     <Footer/>
+      
+    </div>
+  )
+}
+
+export default App
